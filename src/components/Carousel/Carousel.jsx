95bc0fb128db4f04e98b0dc131a5c8f6a6dab778
kartik-mindstix <kartik.vora@mindstix.com>
2025-01-06 11:47:51 +0530

    Implemented carousel slide after every 3 seconds

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,50 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 import "./Carousel.css";
 
 const Carousel = ({ data }) => {
-	const [slide, setSlide] = useState(0);
+  const [slide, setSlide] = useState(0);
 
-	const nextSlide = () => {
-		setSlide(slide === data.length - 1 ? 0 : slide + 1);
-	};
+  const nextSlide = () => {
+    setSlide(slide === data.length - 1 ? 0 : slide + 1);
+  };
 
-	const prevSlide = () => {
-		setSlide(slide === 0 ? data.length - 1 : slide - 1);
-	};
+  const prevSlide = () => {
+    setSlide(slide === 0 ? data.length - 1 : slide - 1);
+  };
 
-	return (
-		<div className="carousel">
-			<BsArrowLeftCircleFill onClick={prevSlide} className="arrow arrow-left" />
-			{data.map((item, idx) => {
-				return (
-					<img
-						src={item.src}
-						alt={item.alt}
-						key={idx}
-						className={slide === idx ? "slide" : "slide slide-hidden"}
-					/>
-				);
-			})}
-			<BsArrowRightCircleFill
-				onClick={nextSlide}
-				className="arrow arrow-right"
-			/>
-			<span className="indicators">
-				{data.map((_, idx) => {
-					return (
-						<button
-							key={idx}
-							className={
-								slide === idx ? "indicator" : "indicator indicator-inactive"
-							}
-							onClick={() => setSlide(idx)}
-						></button>
-					);
-				})}
-			</span>
-		</div>
-	);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [slide]);
+
+  return (
+    <div className="carousel">
+      <BsArrowLeftCircleFill onClick={prevSlide} className="arrow arrow-left" />
+      {data.map((item, idx) => {
+        return (
+          <img
+            src={item.src}
+            alt={item.alt}
+            key={idx}
+            className={slide === idx ? "slide" : "slide slide-hidden"}
+          />
+        );
+      })}
+      <BsArrowRightCircleFill onClick={nextSlide} className="arrow arrow-right" />
+      <span className="indicators">
+        {data.map((_, idx) => {
+          return (
+            <button
+              key={idx}
+              className={
+                slide === idx ? "indicator" : "indicator indicator-inactive"
+              }
+              onClick={() => setSlide(idx)}
+            ></button>
+          );
+        })}
+      </span>
+    </div>
+  );
 };
 
 export default Carousel;
